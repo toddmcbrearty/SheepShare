6972cfc7e368c672aaf67b90f5ca9f6d68c2a4be
todd mc brearty <todd.mcbrearty@gmail.com>
2019-05-30 17:13:45 -0400

    added heading

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
-    <sheep-share :data="{url, via: 'onclive'}"></sheep-share>
+    <div>
+        <h3>SheepShare<br /><small>Sharing componet for the sheep the share</small></h3>
+
+        <sheep-share :data="{url, via: 'onclive'}"></sheep-share>
+    </div>
 </template>
 
 <script>
-import SheepShare from './components/SheepShare';
+  import SheepShare from './components/SheepShare';
 
-export default {
-  name: 'app',
-  components: {
-    SheepShare,
-  },
-  data() {
-    return {
-      url: 'https://www.onclive.com/publications/oncology-live/2018/vol-19-no-24/tucatinib-shows-promise-in-patients-with-breast-cancer-and-brain-metastases?utm_source=onclive&utm_medium=HPbackdrop&utm_campaign=tucatinib'
+  export default {
+    name: 'app',
+    components: {
+      SheepShare,
+    },
+    data() {
+      return {
+        url: 'https://www.onclive.com/publications/oncology-live/2018/vol-19-no-24/tucatinib-shows-promise-in-patients-with-breast-cancer-and-brain-metastases?utm_source=onclive&utm_medium=HPbackdrop&utm_campaign=tucatinib'
+      };
     }
-  }
-}
+  };
 </script>
 
 <style lang="scss">
+    h3 {
+        small {
+            font-weight: normal;
+        }
+    }
 </style>
